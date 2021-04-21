989a75ad76ce9aaecd29045977737e329e3b958c
pdemz <pdems@icloud.com>
2021-04-21 11:10:34 -0700

    Update AppDelegate.swift

--- Yokwe2/AppDelegate.swift
+++ Yokwe2/AppDelegate.swift
@@ -22,11 +22,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyCqZ50c_NV-tYwVOMwxaS4XY-vomaxsOEc")
-        GMSPlacesClient.provideAPIKey("AIzaSyD5aRAiNtUOfUvYplUIiXxtxApicvoQ1p8")
+        GMSServices.provideAPIKey("###")
+        GMSPlacesClient.provideAPIKey("###")
         
-        //Stripe.setDefaultPublishableKey("pk_live_bd9UGPnXebRwYRUCsz7a3F5e")
-        STPPaymentConfiguration.shared().publishableKey = "pk_live_bd9UGPnXebRwYRUCsz7a3F5e"
+        //Stripe.setDefaultPublishableKey("###")
+        STPPaymentConfiguration.shared().publishableKey = "###"
         
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
  
